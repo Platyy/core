b6fa5d6de3b63be575d99f2e4aa34176a16b60b9
Nick <nicholasdallafiore>
2016-09-13 09:56:14 +1000

    + Added ColourSuite and a main menu system

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrbit : MonoBehaviour {
+
+    public Transform m_Center;
+
+    public float m_RotationSpeed = 50f;
+
+    void Update()
+    {
+        transform.RotateAround(m_Center.position, Vector3.up, m_RotationSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using InControl;
+
+public class MainMenuManager : MonoBehaviour {
+
+    private GameObject m_EventSystem;
+
+    private enum SelectedButton
+    {
+        PLAYBUTTON,
+        OPTIONSBUTTON,
+        EXITBUTTON
+    }
+
+    private SelectedButton m_CurrentButton;
+
+    void Start()
+    {
+        m_EventSystem = GameObject.Find("EventSystem");
+        m_EventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("PlayButton"));
+        m_CurrentButton = SelectedButton.PLAYBUTTON;
+    }
+
+    void Update()
+    {
+            switch (m_CurrentButton)
+            {
+                case SelectedButton.PLAYBUTTON:
+
+                    if(InputManager.ActiveDevice.DPadDown.WasPressed)
+                    {
+                        m_EventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("OptionsButton"));
+                        m_CurrentButton = SelectedButton.OPTIONSBUTTON;
+                    }
+                    else if(InputManager.ActiveDevice.DPadUp.WasPressed)
+                    {
+                        break;
+                    }
+                    break;
+
+                case SelectedButton.OPTIONSBUTTON:
+                    if (InputManager.ActiveDevice.DPadUp.WasPressed)
+                    {
+                        m_EventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("PlayButton"));
+                        m_CurrentButton = SelectedButton.PLAYBUTTON;
+                    }
+                    else if (InputManager.ActiveDevice.DPadDown.WasPressed)
+                    {
+                        m_EventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("ExitButton"));
+                        m_CurrentButton = SelectedButton.EXITBUTTON;
+                    }
+                    break;
+
+                case SelectedButton.EXITBUTTON:
+                    if (InputManager.ActiveDevice.DPadDown.WasPressed)
+                    {
+                        break;
+                    }
+                    else if (InputManager.ActiveDevice.DPadUp.WasPressed)
+                    {
+                        m_EventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("OptionsButton"));
+                        m_CurrentButton = SelectedButton.OPTIONSBUTTON;
+                    }
+                    break;
+            }
+    }
+
+    public void ClickPlay()
+    {
+        SceneManager.LoadScene("SymmetricalLayout1", LoadSceneMode.Single);
+    }
+
+    public void ClickOptions()
+    {
+
+
+    }
+
+    public void ClickQuit()
+    {
+        Application.Quit();
+    }
+
+
+
+}
