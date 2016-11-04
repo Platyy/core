7a40ec7217461e9f1791973bee627e24349eeb42
AIE\bradley.wilson <bradleywilsonj@gmail.com>
2016-11-04 14:52:05 +1100

    + Tweaked Tile Emissive Values + Created Two Possible Spawn Particle Effects

---
+++ Assets/ParticleEffects/SpawnParticle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnParticle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, 0, 2);
+	
+	}
+}
